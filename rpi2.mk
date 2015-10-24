121a41b3b229eada2e8e527ccee2bebec6587975
Peter Yoon <peyo.hd@gmail.com>
2015-10-24 11:53:50 +0900

    enable vc4 gpu

--- rpi2.mk
+++ rpi2.mk
@@ -12,7 +12,9 @@ PRODUCT_AAPT_PREF_CONFIG := tvdpi
 include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 
 PRODUCT_PACKAGES += \
-    libGLES_android
+    libGLES_mesa \
+    gralloc.$(TARGET_PRODUCT) \
+    hwcomposer.$(TARGET_PRODUCT)
 
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
